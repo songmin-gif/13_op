465523d1ff66e8c4d34071d70ea30f0586d32a3c
songmin-gif <55427684+songmin-gif@users.noreply.github.com>
2019-11-26 15:22:46 +0900

    practice1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student {
+	int 	ID;
+	char name[7];
+	double grade;
+	
+};
+
+int main (void) {
+	struct student s1={19, "songmin", 4.3};
+	return 0;
+	
+}
